90a29c38af6641fe3ee777e8575521cc8e3774ef
Combifightet <malte@hei-sharp-s.de>
2024-10-22 17:33:58 +0200

    fixed (not-) equal uperators

--- lib/src/expr/operators/not_equal.dart
+++ lib/src/expr/operators/not_equal.dart
@@ -10,7 +10,7 @@ class NotEqual extends Operator {
   bool getValue(FolWorld world, Map<String, LogicObj> variables) {
     ExpressionTree left =  node?.getLeft() as ExpressionTree;
     ExpressionTree right =  node?.getRight() as ExpressionTree;
-    return left.getData()?.getValue(world, variables) != right.getData()?.getValue(world, variables);
+    return left.getData()?.getValue(world, variables)[0] != right.getData()?.getValue(world, variables)[0];
   }
 
   @override
